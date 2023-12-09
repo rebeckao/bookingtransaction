ae5439704b12a30a9384dbc7eac030151318e89a
rebeckao <rebecka.oefverholm@gmail.com>
2023-12-09 13:53:11 +0100

    Failing test

--- src/test/java/dev/rebeckao/bookingtransaction/IntegrationTests.java
+++ src/test/java/dev/rebeckao/bookingtransaction/IntegrationTests.java
@@ -1,12 +1,13 @@
 package dev.rebeckao.bookingtransaction;
 
+import dev.rebeckao.bookingtransaction.model.Transaction;
 import dev.rebeckao.bookingtransaction.model.TransactionResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -21,12 +22,21 @@ public class IntegrationTests {
 
         client.post()
                 .uri("/process-transactions")
-                .bodyValue("")
+                .bodyValue("""
+                        "John,Doe,john@doe.com,190,TR0001"
+                        "John,Doe1,john@doe1.com,200,TR0001"
+                        "John,Doe2,john@doe2.com,201,TR0003"
+                        "John,Doe,john@doe.com,9,TR0004"
+                        "John,Doe,john@doe.com,2,TR0005"
+                        """)
                 .exchange()
                 .expectStatus()
                 .isOk()
                 .expectBody(TransactionResponse.class)
-                .isEqualTo(new TransactionResponse(Collections.emptyList()));
+                .isEqualTo(new TransactionResponse(List.of(
+                        new Transaction("John", "Doe2", "john@doe2.com", "TR0003"),
+                        new Transaction("John", "Doe", "john@doe.com", "TR0005")
+                )));
 
     }
 }
