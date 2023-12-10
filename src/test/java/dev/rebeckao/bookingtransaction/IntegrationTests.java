abc7aaca333b6a2a11643636255fd450c5b92cf8
rebeckao <rebecka.oefverholm@gmail.com>
2023-12-10 22:00:11 +0100

    More test cases

--- src/test/java/dev/rebeckao/bookingtransaction/IntegrationTests.java
+++ src/test/java/dev/rebeckao/bookingtransaction/IntegrationTests.java
@@ -3,11 +3,13 @@ package dev.rebeckao.bookingtransaction;
 import dev.rebeckao.bookingtransaction.model.RejectedTransaction;
 import dev.rebeckao.bookingtransaction.model.TransactionResponse;
 import dev.rebeckao.bookingtransaction.persistence.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -17,14 +19,72 @@ public class IntegrationTests {
     @LocalServerPort
     private int port;
 
-    @Test
-    void processSuccessfulAndFailedTransactions() {
-        WebTestClient client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
+    private WebTestClient client;
 
+    @BeforeEach
+    void setUp() {
+        client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
         client.delete()
                 .uri("/persisted-data")
                 .exchange();
+    }
 
+    @Test
+    void processTransactionForUserWithCreditLimit() {
+        client.put()
+                .uri("/set-credit-limits")
+                .bodyValue(List.of(new UserEntity("john@doe.com", 200)))
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        client.post()
+                .uri("/process-transactions")
+                .bodyValue("John,Doe,john@doe.com,190,TR0001")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(TransactionResponse.class)
+                .isEqualTo(new TransactionResponse(Collections.emptyList()));
+    }
+
+    @Test
+    void processTransactionForUserWithoutCreditLimit() {
+        client.post()
+                .uri("/process-transactions")
+                .bodyValue("John,Doe,john@doe.com,190,TR0001")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(TransactionResponse.class)
+                .isEqualTo(new TransactionResponse(List.of(
+                        new RejectedTransaction("John", "Doe", "john@doe.com", "TR0001")
+                )));
+    }
+
+    @Test
+    void processTransactionForUserWithoutInsufficientCreditLimit() {
+        client.put()
+                .uri("/set-credit-limits")
+                .bodyValue(List.of(new UserEntity("john@doe.com", 100)))
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        client.post()
+                .uri("/process-transactions")
+                .bodyValue("John,Doe,john@doe.com,190,TR0001")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(TransactionResponse.class)
+                .isEqualTo(new TransactionResponse(List.of(
+                        new RejectedTransaction("John", "Doe", "john@doe.com", "TR0001")
+                )));
+    }
+
+    @Test
+    void processSuccessfulAndFailedTransactions() {
         client.put()
                 .uri("/set-credit-limits")
                 .bodyValue(List.of(
@@ -53,6 +113,5 @@ public class IntegrationTests {
                         new RejectedTransaction("John", "Doe2", "john@doe2.com", "TR0003"),
                         new RejectedTransaction("John", "Doe", "john@doe.com", "TR0005")
                 )));
-
     }
 }
